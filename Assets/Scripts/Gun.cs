751c868807fea13806af68d98100fb01b28d3359
MoMakkawi <mohamad.makkawi@outlook.com>
2023-07-28 21:31:47 +0300

    Add Gun sound effect

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -7,10 +7,18 @@ public class Gun : MonoBehaviour
     public float BulletSpeed = 40f;
     public static float BulletsNumbes;
 
+    public AudioClip bulletSound;
+
     private void Start()
     {
         BulletsNumbes = 70f;
         CountdownTimer.BulletRemaining = BulletsNumbes;
+
+        // Get the AudioSource component attached to this GameObject
+        AudioSource audioSource = GetComponent<AudioSource>();
+
+        // Assign the bulletSound AudioClip to the AudioSource
+        audioSource.clip = bulletSound;
     }
 
     // Update is called once per frame
@@ -24,6 +32,11 @@ public class Gun : MonoBehaviour
         {
             var bullet = Instantiate(BulletPerfab, BulletSpawnPoint.position, BulletSpawnPoint.rotation);
             bullet.GetComponent<Rigidbody>().velocity = BulletSpawnPoint.forward * BulletSpeed;
+
+            // If you are instantiating bullets programmatically, play the audio here
+            AudioSource audioSource = GetComponent<AudioSource>();
+            audioSource.Play();
+
             BulletsNumbes--;
             CountdownTimer.BulletRemaining = BulletsNumbes;
         }
